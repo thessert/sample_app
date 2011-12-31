9ee2c180bbe9363639bfeb02d379182c0595d566
thessert <thessert@gmail.com>
2011-12-31 10:07:22 -0500

    added microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,11 @@ flash[:success] = "User destroyed."
 redirect_to users_path
 end
 
+def show
+@user = User.find(params[:id])
+@microposts = @user.microposts.paginate(:page => params[:page])
+@title = @user.name
+end
 
 private
 def authenticate
